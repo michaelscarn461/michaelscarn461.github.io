6cae39f261a4a26082a20c9418d41eda1e8eb373
Nachiket Trivedi <nachikettrivedi@Nachikets-MacBook-Air.local>
2020-08-31 19:40:20 -0700

    Updates

---
+++ precache-manifest.e863b997310c0f1dd236d5926173cd1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7278ddc4f1247432a6cb897cdab6a2a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "87131c1c5cfcf6f69674",
+    "url": "/static/css/main.f60eae6a.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87131c1c5cfcf6f69674",
+    "url": "/static/js/main.c4750a2d.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
